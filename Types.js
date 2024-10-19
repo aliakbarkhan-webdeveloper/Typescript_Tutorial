32076fb38c6969e7ca28ae7d195184219a20ced8
Ali Akbar Khan <122238636+aliakbarkhan-webdeveloper@users.noreply.github.com>
2024-10-19 23:06:28 +0500

    commit

---
+++ Types.js
@@ -0,0 +1,33 @@
+//Numbers
+var a;
+a = 13;
+console.log(a);
+//boolean
+//undefine
+//string
+//characters
+//symbols
+//Arrays
+var arr;
+//Tuples
+var tuple = [5, "hj"];
+//Any
+var an;
+an = true;
+console.log(typeof (an));
+//Unknown
+var un;
+un = "!2";
+console.log(typeof un);
+//Never
+//Void
+//Enum
+var myData;
+(function (myData) {
+    myData["name"] = "Ali Akbar";
+    myData[myData["age"] = 24] = "age";
+    myData["Qualifications"] = "Bachelors";
+})(myData || (myData = {}));
+console.log(myData);
+//Objects
+//Classes
--- Types.ts
+++ Types.ts
@@ -1,58 +1,47 @@
-
 //Numbers
-var a:number;
-a=13;
+var a: number;
+a = 13;
 console.log(a);
 
 //boolean
 
-
 //undefine
-
 
 //string
 
-
 //characters
-
 
 //symbols
 
-
 //Arrays
-let arr:[];
-
+let arr: [];
 
 //Tuples
-let tuple:[number,string]=[5,"hj"];
+let tuple: [number, string] = [5, "hj"];
 
 //Any
-
-
-
+let an:any;
+an=true;
+console.log(typeof(an));
 
 //Unknown
 
-
-
+let un: unknown;
+un = "!2";
+console.log(typeof un);
 
 //Never
 
-
-
 //Void
-
-
-
 
 //Enum
 
+enum myData {
+  name = "Ali Akbar",
+  age = 24,
+  Qualifications = "Bachelors",
+}
+
+console.log(myData);
 
 
-//Objects
-
-
-//Classes
-
-
-//Tuples

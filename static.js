b17001ab7541a0b7340a37a9921c6377d5cc328a
Ali Akbar Khan <122238636+aliakbarkhan-webdeveloper@users.noreply.github.com>
2024-10-21 23:52:03 +0500

    static

---
+++ static.js
@@ -0,0 +1,8 @@
+"use restrict";
+var Myapp = /** @class */ (function () {
+    function Myapp() {
+    }
+    Myapp.version = 1.0;
+    return Myapp;
+}());
+console.log(Myapp.version);
---
+++ static.ts
@@ -0,0 +1,8 @@
+"use restrict"
+class Myapp{
+    public static version:number=1.0;
+
+}
+
+
+console.log(Myapp.version);

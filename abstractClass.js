ee52d3eb129798c2a10065bd28ad4b7179d4e155
Ali Akbar Khan <122238636+aliakbarkhan-webdeveloper@users.noreply.github.com>
2024-10-22 17:26:50 +0500

    Abstract Classes

--- abstractClass.js
+++ abstractClass.js
@@ -1,1 +1,60 @@
 "use strict";
+var __extends =
+  (this && this.__extends) ||
+  (function () {
+    var extendStatics = function (d, b) {
+      extendStatics =
+        Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array &&
+          function (d, b) {
+            d.__proto__ = b;
+          }) ||
+        function (d, b) {
+          for (var p in b)
+            if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p];
+        };
+      return extendStatics(d, b);
+    };
+    return function (d, b) {
+      if (typeof b !== "function" && b !== null)
+        throw new TypeError(
+          "Class extends value " + String(b) + " is not a constructor or null"
+        );
+      extendStatics(d, b);
+      function __() {
+        this.constructor = d;
+      }
+      d.prototype =
+        b === null
+          ? Object.create(b)
+          : ((__.prototype = b.prototype), new __());
+    };
+  })();
+var Tree = /** @class */ (function () {
+  function Tree(leaf, root) {
+    this.leaf = leaf;
+    this.root = root;
+  }
+  return Tree;
+})();
+var FruitTree = /** @class */ (function (_super) {
+  __extends(FruitTree, _super);
+  function FruitTree(fruit, leaf, root) {
+    var _this = _super.call(this, leaf, root) || this;
+    _this.fruit = fruit;
+    _this.leaf = leaf;
+    _this.root = root;
+    _this.fruit = fruit;
+    return _this;
+  }
+  FruitTree.prototype.getData = function () {
+    console.log(
+      "This is "
+        .concat(this.fruit, " tree with ")
+        .concat(this.leaf, " leaves and ")
+        .concat(this.root, " roots")
+    );
+  };
+  return FruitTree;
+})(Tree);
+new FruitTree("Mango", "50", "countless").getData();
--- abstractClass.ts
+++ abstractClass.ts
@@ -1,2 +1,20 @@
-"use strict"
+"use strict";
 
+class Tree {
+  constructor(public leaf: string, public root: string) {}
+}
+
+class FruitTree extends Tree {
+  constructor(public fruit: string, public leaf: string, public root: string) {
+    super(leaf, root);
+    this.fruit = fruit;
+  }
+
+  getData() {
+    console.log(
+      `This is ${this.fruit} tree with ${this.leaf} leaves and ${this.root} roots`
+    );
+  }
+}
+
+new FruitTree("Mango", "50", "countless").getData();

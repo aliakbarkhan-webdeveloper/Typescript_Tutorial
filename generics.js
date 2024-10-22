ab6d8b9ab0ea17f83dc69195999a01b7c1aa136b
Ali Akbar Khan <122238636+aliakbarkhan-webdeveloper@users.noreply.github.com>
2024-10-22 21:07:16 +0500

    Generic

--- generics.js
+++ generics.js
@@ -4,3 +4,7 @@ function funcGen(a, b) {
     console.log("the type of generic is ".concat(typeof a, " and the other data type is ").concat(typeof b));
 }
 funcGen(false, "true");
+function employee(obj) {
+    console.log("\n    The type of name is ".concat(typeof obj.name, "\n    The type of age is ").concat(typeof obj.age, "\n    The type of key is ").concat(typeof obj.key, "\n    "));
+}
+employee({ name: "ali", age: 24, key: "akdkasd7wdas" });
--- generics.ts
+++ generics.ts
@@ -1,10 +1,28 @@
-`use strict`
+`use strict`;
 
 //function generic
 
-function funcGen <T> (a:T,b:string):void{
+function funcGen<T>(a: T, b: string): void {
+  console.log(
+    `the type of generic is ${typeof a} and the other data type is ${typeof b}`
+  );
+}
+funcGen<boolean>(false, "true");
 
-    console.log(`the type of generic is ${typeof a} and the other data type is ${typeof b}`);
-    
+//Generic Interface
 
-}funcGen <boolean>(false,"true");
+interface employData<T> {
+  name: string;
+  age: number;
+  key: T;
+}
+
+function employee(obj: employData<string>) {
+  console.log(`
+    The type of name is ${typeof obj.name}
+    The type of age is ${typeof obj.age}
+    The type of key is ${typeof obj.key}
+    `);
+}
+
+employee({name:"ali",age:24,key:"akdkasd7wdas"});
